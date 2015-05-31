9966944e67ce76b23c2cda105e8e0cc3a1d7b22c
Krister Vals <krister.vals@gmail.com>
2015-05-31 22:55:27 +0300

    refactored with no case argument

--- case_statement.rb
+++ case_statement.rb
@@ -1,11 +1,11 @@
-# case_statement.rb <-- refactored
+# case_statement.rb <-- refactored with no case argument
 
 a = 5
 
-answer = case a
-  when 5
+answer = case
+  when a == 5
     "a is 5"
-  when 6
+  when a == 6
     "a is 6"
   else
     "a is neither 5, nor 6"
