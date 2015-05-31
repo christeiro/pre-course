8073b8661ba9a3d30a6a67476696dc010a095a75
Krister Vals <krister.vals@gmail.com>
2015-05-31 20:03:12 +0300

    Mutate - method return

--- mutate.rb
+++ mutate.rb
@@ -1,12 +1,11 @@
-# Example of a method that does not mutate the caller
 # mutate.rb
 
 a = [1, 2, 3]
 
-def no_mutate(array)
-  array.last
+def mutate(array)
+  array.pop
 end
 
-p "Before no_mutate method: #{a}"
-no_mutate(a)
-p "After no_mutate method: #{a}"
+p "Before mutate method: #{a}"
+p mutate(a)
+p "After mutate method: #{a}"
