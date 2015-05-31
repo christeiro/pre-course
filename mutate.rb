6caa8aa38c83bc85c41233a98ff5295af72b4db4
Krister Vals <krister.vals@gmail.com>
2015-05-31 20:01:14 +0300

    No mutate

--- mutate.rb
+++ mutate.rb
@@ -1,12 +1,12 @@
-# Example of a method that modifies its argument permanently
+# Example of a method that does not mutate the caller
 # mutate.rb
 
 a = [1, 2, 3]
 
-def mutate(array)
-  array.pop
+def no_mutate(array)
+  array.last
 end
 
-p "Before mutate method: #{a}"
-mutate(a)
-p "After mutate method: #{a}"
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
